b38808aa7f8cf37db114a2cf2759969053e0712d
punk0706 <punk0706@gmail.com>
2014-09-26 12:17:48 -0400

    gomoku unit test
    this is a commit for SMG hw2 unit test

---
+++ karma.conf.js
@@ -0,0 +1,44 @@
+module.exports = function(config){
+  config.set({
+
+    basePath : './',
+
+    files : [
+      '*.js'
+    ],
+
+    reporters: ['progress', 'coverage'],
+
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'gomokuLogic.js': ['coverage']
+    },
+
+    // optionally, configure the reporter
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome'],
+
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine',
+            'karma-coverage'
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  });
+};
