8823a3505ac46232760bb774dbaca867c696d143
kevinfguo <blindspotx2@gmail.com>
2015-12-26 16:34:28 -0500

    Almost there...
    Can't quite upgrade the AI from JS to TS...

---
+++ src/game.ts
@@ -0,0 +1,255 @@
+// 'use strict';
+//
+// angular.module('myApp', ['ngTouch'])
+//   .controller('Ctrl', function (
+//        $window, $scope, $log, $timeout,
+//       aiService, gameService, scaleBodyService, gameLogic) {
+
+module game{
+
+  'use strict';
+  var board : any;
+  var delta : any;
+  var numOfMoves : any;
+  var isAiWorking : any;
+  var isYourTurn : any;
+  var turnIndex: any;
+  var newMove : any;
+  var isFinished : any;
+
+
+  var moveAudio = new Audio('audio/move.wav');
+  moveAudio.load();
+
+  // iniAiService();
+
+  export function init() {
+    console.log("Translation of 'RULES_OF_OWARE' is " + translate('RULES_OF_OWARE'));
+    resizeGameAreaService.setWidthToHeight(1);
+    gameService.setGame({
+      minNumberOfPlayers: 2,
+      maxNumberOfPlayers: 2,
+      isMoveOk: gameLogic.isMoveOk,
+      updateUI: updateUI
+    });
+  }
+
+  export function updateUI(params : any) {
+    console.log("Game got updateUI:", params);
+    board = params.stateAfterMove.board;
+    delta = params.stateAfterMove.delta;
+    if (board === undefined) {
+    	numOfMoves = 0;
+    	isAiWorking = false;
+      board = [['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+	           		['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+	           		['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+	           		['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+	           		['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+	           		['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+	           		['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+	           		['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+	           		['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+	           		['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+	           		['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+	           		['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+	           		['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+	           		['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+	           		['', '', '', '', '', '', '', '', '', '', '', '', '', '', '']];
+    } else {
+      // Only play a sound if there was a move (i.e., state is not empty).
+      moveAudio.play();
+    }
+  	isYourTurn = params.turnIndexAfterMove >= 0 && // game is ongoing
+    params.yourPlayerIndex === params.turnIndexAfterMove; // it's my turn
+    turnIndex = params.turnIndexAfterMove;
+    if (isYourTurn && params.playersInfo[params.yourPlayerIndex].playerId === '') {
+    // Wait 500 milliseconds until animation ends.
+    	$timeout(sendComputerMove, 600);
+  	}
+  }
+
+    export function sendComputerMove() {
+        var aimove : any = [];
+        if(numOfMoves < 2){
+        	aimove = firstAIMoveGenerator();
+        }
+        else{
+        	aimove = aiServiceMakeMove();
+        }
+        newMove = aimove;
+        gameService.makeMove(gameLogic.createMove(board, aimove[0], aimove[1], turnIndex));
+        //isFinished = updateMessage(gameLogic.createMove(board, aimove[0], aimove[1], turnIndex));
+        aiService.informingComputer(aimove[0], aimove[1], 'white');
+        $timeout(updateAIStatues, 500);
+        numOfMoves++;
+    }
+
+    export function updateAIStatues(){
+        isAiWorking = false;
+        // if (!isFinished){
+        // 	document.getElementById("gamemsg").innerHTML = "Black's turn";
+        // }
+    }
+
+    updateUI({stateAfterMove: {}, turnIndexAfterMove: 0, yourPlayerIndex: -2});
+
+    export function iniAiService(){
+    	aiService.iniComputer('hard');
+    }
+
+    export function aiServiceMakeMove(){
+    	return aiService.getMove();
+    }
+
+    export function firstAIMoveGenerator(){
+    	var moves=[
+            [6,6],
+            [6,7],
+            [6,8],
+            [7,6],
+            [7,7],
+            [7,8],
+            [8,6],
+            [8,7],
+            [8,8]
+        ];
+        while(true){
+            var ind=Math.floor(Math.random()*moves.length);
+            if(board[moves[ind][0]][moves[ind][1]] === ''){
+                return [(moves[ind][0]),(moves[ind][1])];
+            }else{
+                moves.splice(ind,1);
+            }
+        }
+    }
+    /*
+    function sendMakeMove(move) {
+      $log.info(["Making move:", move]);
+      if (isLocalTesting) {
+        stateService.makeMove(move);
+      } else {
+        messageService.sendMessage({makeMove: move});
+      }
+      if('endMatch' in move[0]){
+				var score = move[0].endMatch.endMatchScores;
+				if(score[0] > score[1]){
+					$window.document.getElementById("gamemsg").innerHTML = "Game over, Black Wins";
+				}
+				else if(score[0] < score[1]){
+					$window.document.getElementById("gamemsg").innerHTML = "Game over, White Wins";
+				}
+				else{
+					$window.document.getElementById("gamemsg").innerHTML = "Game over, Ties";
+				}
+				setTimeout(function(){$window.document.getElementById("alertbox").style.display = "block";}, 1000);
+			}
+    }
+    */
+    // export function updateMessage(move : any){
+    //   if('endMatch' in move[0]){
+  	// 			var score = move[0].endMatch.endMatchScores;
+  	// 			if(score[0] > score[1]){
+  	// 				document.getElementById("gamemsg").innerHTML = "Black Wins";
+  	// 			}
+  	// 			else if(score[0] < score[1]){
+  	// 				document.getElementById("gamemsg").innerHTML = "White Wins";
+  	// 			}
+  	// 			else{
+  	// 				document.getElementById("gamemsg").innerHTML = "Ties";
+  	// 			}
+  	// 			document.getElementById("newgamebt").style.display = "block";
+  	// 			return true;
+  	// 	}
+  	// 	return false;
+    // }
+
+    export function placeDot (str : any, row : any, col : any){
+
+      var piece = board[row][col];
+      if(piece ===''){
+      	return 'img/empty.png';
+      	//return 0;
+      }
+      if(piece === 'X'){
+      	if(row === newMove[0] && col === newMove[1]){
+      		return 'img/newblackStone.png';
+      	}
+      	return 'img/blackStone.png';
+      }
+      if(piece === 'O'){
+      	if(row === newMove[0] && col === newMove[1]){
+      		return 'img/newwhiteStone.png';
+      	}
+      	return 'img/whiteStone.png'
+      }
+    }
+
+    export function shouldSlowlyAppear(row : any, col : any) {
+      return delta !== undefined
+          && delta.row === row && delta.col === col;
+    }
+
+    export function cellClicked(row : any, col : any) {
+      log.info(["Clicked on cell:", row, col]);
+      if (!isYourTurn) {
+        return;
+      }
+      try {
+        if (!isAiWorking){
+          // isAiWorking = true;
+          var move = gameLogic.createMove(board, row, col, turnIndex);
+          newMove = [row, col];
+          isYourTurn = false; // to prevent making another move
+          gameService.makeMove(move);
+          //isFinished = updateMessage(move);
+          numOfMoves++;
+          // aiService.informingComputer(row, col, 'black');
+          // log.info("got here");
+          // if(!isFinished){
+          //   document.getElementById("gamemsg").innerHTML = "AI thinking...";
+          // }
+        }
+        else{
+        	return false;
+        }
+      } catch (e) {
+        log.info(e);
+        log.info(["Cell is already full in position:", row, col]);
+        return;
+      }
+    };
+
+    export function onStartCallback() {
+      log.info("onStart happened!", arguments);
+    };
+
+
+  //scaleBodyService.scaleBody({width: 1200, height: 1200});
+  //   gameService.setGame({
+  //     gameDeveloperEmail: "punk0706@gmail.com",
+  //     minNumberOfPlayers: 2,
+  //     maxNumberOfPlayers: 2,
+  //     exampleGame: gameLogic.getExampleGame(),
+  //     riddles: gameLogic.getRiddles(),
+  //     isMoveOk: gameLogic.isMoveOk,
+  //     updateUI: updateUI
+  //   });
+  // });
+}
+
+angular.module('myApp', ['ngTouch', 'ui.bootstrap', 'gameServices'])
+  .run(function () {
+  $rootScope['game'] = game;
+  translate.setLanguage('en',  {
+    RULES_OF_OWARE:"Rules of OWARE",
+  	RULES_SLIDE1: "To sow you must take all the seeds of  any of your holes and lay its out along the holes against the direction of the clockwise. In every hole you should lay it out one seed.  If you reach the last hole of your ground you must continue in the land of the other player. Remember, you always have to lay out seeds in the direction against the clockwise.",
+  	RULES_SLIDE2: "If the last hole where you sow is in the land of the other player and there are two or three seeds in the last hole remove from the board and keep them. If the previous holes also contain two or three seeds also remove them and remove all the seeds of your opponent that contains two or three seeds.",
+  	RULES_SLIDE3: "As the game progresses, it is possible that one hole contains more than 12 seeds. This hole is called Kroo and makes possible complete one round. When the harvest starts at the Kroo, this hole must finish empty what means that the player shouldn’t lay out any seed.",
+  	RULES_SLIDE4: "To play a run  and  leaves the other player with no seeds to continue playing is not allowed. If you do it you will lose the game.",
+  	RULES_SLIDE5: "If the other player has only one seed in his field you will have to remove it in order to harvest and continue playing. Players must provide in advance to avoid this situation. If this is impossible, because we only have one seed in our land. The game is finished. The winner is the one that harvest more.",
+  	RULES_SLIDE6: "Game ends with a player having more than 23 seeds in his house. When there are few seeds left on the counter, the game may be perpetuating and hardly any of the 2 players can capture any new seed. By mutual agreement player can agree the end of the game. In this case every player is the owner of the seeds in his side.  As always, who has garnered more wins the match.",
+  	CLOSE:"Close"
+  });
+  game.init();
+});
---
+++ ts_output_readonly_do_NOT_change_manually/src/aiService.js
@@ -0,0 +1,382 @@
+// 'use strict';
+// angular.module('myApp').service('aiService', function() {
+var aiService;
+(function (aiService) {
+    'use strict';
+    var mapPoint = (function () {
+        function mapPoint(r, c) {
+            r = r;
+            c = c;
+            this.set = false;
+            this.score = 0;
+            this.valid = false;
+            this.info = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
+        }
+        return mapPoint;
+    })();
+    // var ai={};
+    var sum = 0;
+    var setNum = 0;
+    var scopeMap = [];
+    var scorequeue = [];
+    var map = [];
+    var moves = [
+        [-1, -1],
+        [-1, 0],
+        [0, -1],
+        [-1, 1]
+    ];
+    var coe = [-2, 1];
+    var scores = [0, 1, 10, 2000, 4000, 100000000000];
+    var cache;
+    var otc;
+    var depth;
+    var totry;
+    var myColor;
+    for (var i = 0; i < 15; i++) {
+        var tmp = [];
+        for (var j = 0; j < 15; j++) {
+            var a = new mapPoint(i, j);
+            tmp.push(a);
+            scorequeue.push(a);
+        }
+        map.push(tmp);
+    }
+    var boardBuf = new ArrayBuffer(255);
+    var boardBufArr = new Uint8Array(boardBuf);
+    function bufToString() {
+        return String.fromCharCode.apply(null, boardBufArr);
+    }
+    aiService.bufToString = bufToString;
+    function ini(mode, color) {
+        myColor = color;
+        if (color == 'black') {
+            otc = 'white';
+        }
+        else {
+            otc = 'black';
+        }
+        switch (mode) {
+            case 'easy':
+                depth = 5;
+                totry = [12, 8];
+                break;
+            case 'hard':
+                depth = 7;
+                totry = [10, 10];
+                break;
+            default:
+                console.log('ini erro');
+        }
+        console.log('ini complete');
+    }
+    aiService.ini = ini;
+    ;
+    function watch(r, c, color) {
+        console.log("row, col: ", r, c, "color: ", color);
+        updateMap(r, c, color);
+        if (color == 'remove') {
+            setNum--;
+        }
+        else
+            setNum++;
+        scorequeue.sort(sortMove);
+    }
+    aiService.watch = watch;
+    function updateMap(r, c, color) {
+        var remove = false, num;
+        if (color == myColor) {
+            num = 1;
+        }
+        else if (color == otc) {
+            num = 0;
+        }
+        else {
+            remove = true;
+            num = map[r][c].set - 1;
+        }
+        console.log("Got here");
+        return _updateMap(r, c, num, remove);
+    }
+    aiService.updateMap = updateMap;
+    ;
+    function _updateMap(r, c, num, remove) {
+        var i = 4, x, y, step, tmp, xx, yy, cur, changes = 0, s, e;
+        if (!remove) {
+            console.log("Got here: Branch 1");
+            boardBufArr[r * 15 + c] = num + 2;
+            map[r][c].set = num + 1;
+            while (i--) {
+                x = r;
+                y = c;
+                step = 5;
+                while (step-- && x >= 0 && y >= 0 && y < 15) {
+                    xx = x - moves[i][0] * 4;
+                    yy = y - moves[i][1] * 4;
+                    if (xx >= 15 || yy < 0 || yy >= 15) {
+                        x += moves[i][0];
+                        y += moves[i][1];
+                        continue;
+                    }
+                    cur = map[x][y].info[i];
+                    if (cur[2] > 0) {
+                        tmp = 5;
+                        xx = x;
+                        yy = y;
+                        s = scores[cur[2]];
+                        changes -= s * cur[3];
+                        while (tmp--) {
+                            map[xx][yy].score -= s;
+                            xx -= moves[i][0];
+                            yy -= moves[i][1];
+                        }
+                    }
+                    cur[num]++;
+                    if (cur[1 - num] > 0) {
+                        cur[2] = 0;
+                    }
+                    else {
+                        cur[2] = cur[num];
+                        e = coe[num];
+                        cur[3] = e;
+                        s = scores[cur[2]];
+                        tmp = 5;
+                        xx = x;
+                        yy = y;
+                        changes += s * cur[3];
+                        while (tmp--) {
+                            map[xx][yy].score += s;
+                            xx -= moves[i][0];
+                            yy -= moves[i][1];
+                        }
+                    }
+                    x += moves[i][0];
+                    y += moves[i][1];
+                }
+            }
+        }
+        else {
+            console.log("Got here: Branch 2");
+            boardBufArr[r * 15 + c] = 0;
+            map[r][c].set = false;
+            while (i--) {
+                x = r;
+                y = c;
+                step = 5;
+                //others 0 i am 1-> sc=0
+                //others 0 i am more than 1-> sc=1
+                //i am >0 others >0 -> sc=-1
+                while (step-- && x >= 0 && y >= 0 && y < 15) {
+                    xx = x - moves[i][0] * 4;
+                    yy = y - moves[i][1] * 4;
+                    if (xx >= 15 || yy < 0 || yy >= 15) {
+                        x += moves[i][0];
+                        y += moves[i][1];
+                        continue;
+                    }
+                    cur = map[x][y].info[i];
+                    var sc = 0;
+                    cur[num]--;
+                    if (cur[2] > 0) {
+                        tmp = 5;
+                        xx = x;
+                        yy = y;
+                        s = scores[cur[2]];
+                        changes -= s * cur[3];
+                        while (tmp--) {
+                            map[xx][yy].score -= s;
+                            xx -= moves[i][0];
+                            yy -= moves[i][1];
+                        }
+                        cur[2]--;
+                        if (cur[num] > 0)
+                            sc = 1;
+                    }
+                    else if (cur[1 - num] > 0 && !cur[num]) {
+                        sc = -1;
+                    }
+                    if (sc === 1) {
+                        tmp = 5;
+                        s = scores[cur[2]];
+                        xx = x;
+                        yy = y;
+                        changes += s * cur[3];
+                        while (tmp--) {
+                            map[xx][yy].score += s;
+                            //if(!map[xx][yy].set)changes+=s*cur[3];
+                            xx -= moves[i][0];
+                            yy -= moves[i][1];
+                        }
+                    }
+                    else if (sc === -1) {
+                        cur[2] = cur[1 - num];
+                        tmp = 5;
+                        s = scores[cur[2]];
+                        cur[3] = coe[1 - num];
+                        xx = x;
+                        yy = y;
+                        changes += s * cur[3];
+                        while (tmp--) {
+                            map[xx][yy].score += s;
+                            //if(!map[xx][yy].set)changes+=s*cur[3];
+                            xx -= moves[i][0];
+                            yy -= moves[i][1];
+                        }
+                    }
+                    x += moves[i][0];
+                    y += moves[i][1];
+                }
+            }
+        }
+        sum += changes;
+    }
+    aiService._updateMap = _updateMap;
+    ;
+    function simulate(x, y, num) {
+        setNum++;
+        _updateMap(x, y, num, false);
+    }
+    aiService.simulate = simulate;
+    ;
+    function desimulate(x, y, num) {
+        _updateMap(x, y, num, true);
+        setNum--;
+    }
+    aiService.desimulate = desimulate;
+    ;
+    function sortMove(a, b) {
+        if (a.set)
+            return 1;
+        if (b.set)
+            return -1;
+        if (a.score < b.score) {
+            return 1;
+        }
+        else
+            return -1;
+    }
+    aiService.sortMove = sortMove;
+    ;
+    cache = {};
+    function nega(x, y, depth, alpha, beta) {
+        var pt = map[x][y].info, i = 4, num = depth % 2;
+        simulate(x, y, num);
+        var bufstr = bufToString();
+        if (cache[bufstr]) {
+            return cache[bufstr];
+        }
+        if (Math.abs(sum) >= 10000000)
+            return -1 / 0;
+        if (setNum === 225) {
+            return 0;
+        }
+        else if (depth === 0) {
+            return sum;
+        }
+        scorequeue.sort(sortMove);
+        var i = totry[num], tmp, tmpqueue = [], b = beta;
+        while (i--) {
+            tmp = scorequeue[i];
+            if (tmp.set)
+                continue;
+            tmpqueue.push(tmp.c);
+            tmpqueue.push(tmp.r);
+        }
+        depth -= 1;
+        i = tmpqueue.length - 1;
+        x = tmpqueue[i];
+        y = tmpqueue[--i];
+        var score = -nega(x, y, depth, -b, -alpha);
+        desimulate(x, y, depth % 2);
+        if (score > alpha) {
+            bufstr = bufToString();
+            cache[bufstr] = score;
+            alpha = score;
+        }
+        if (alpha >= beta) {
+            bufstr = bufToString();
+            cache[bufstr] = beta;
+            return alpha;
+        }
+        b = alpha + 1;
+        while (i--) {
+            x = tmpqueue[i];
+            y = tmpqueue[--i];
+            score = -nega(x, y, depth, -b, -alpha);
+            desimulate(x, y, depth % 2);
+            if (alpha < score && score < beta) {
+                score = -nega(x, y, depth, -beta, -alpha);
+                desimulate(x, y, depth % 2);
+            }
+            if (score > alpha) {
+                alpha = score;
+            }
+            if (alpha >= beta) {
+                return alpha;
+            }
+            b = alpha + 1;
+        }
+        return alpha;
+    }
+    aiService.nega = nega;
+    ;
+    function move() {
+        cache = {};
+        var alpha = -1 / 0, beta = 1 / 0, bestmove = [scorequeue[0].r, scorequeue[0].c];
+        var i = 20, tmp, tmpqueue = [], depth = depth;
+        while (i--) {
+            tmp = scorequeue[i];
+            if (tmp.score.set)
+                continue;
+            tmpqueue.push(tmp.c);
+            tmpqueue.push(tmp.r);
+        }
+        i = tmpqueue.length - 1;
+        var x, y, b = beta;
+        x = tmpqueue[i];
+        y = tmpqueue[--i];
+        var score = -nega(x, y, depth, -b, -alpha);
+        desimulate(x, y, depth % 2);
+        if (score > alpha) {
+            alpha = score;
+            bestmove = [x, y];
+        }
+        b = alpha + 1;
+        while (i--) {
+            x = tmpqueue[i];
+            y = tmpqueue[--i];
+            score = -nega(x, y, depth, -b, -alpha);
+            desimulate(x, y, depth % 2);
+            if (alpha < score && score < beta) {
+                score = -nega(x, y, depth, -beta, -alpha);
+                desimulate(x, y, depth % 2);
+            }
+            if (score > alpha) {
+                alpha = score;
+                bestmove = [x, y];
+            }
+            b = alpha + 1;
+        }
+        return bestmove;
+    }
+    aiService.move = move;
+    ;
+    function iniComputer(d) {
+        if (d === undefined) {
+            ini('hard', 'white');
+        }
+        else {
+            ini(d, 'white');
+        }
+    }
+    aiService.iniComputer = iniComputer;
+    function informingComputer(r, c, color) {
+        watch(r, c, color);
+    }
+    aiService.informingComputer = informingComputer;
+    function getMove() {
+        var bestmove = move();
+        return bestmove;
+    }
+    aiService.getMove = getMove;
+})(aiService || (aiService = {}));
